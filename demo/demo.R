7b8c09cd0c57111f4a6de1c8aecbced47fd19c1c
Jim Wright <jjwright90@gmail.com>
2020-06-07 09:39:15 -0700

    Demo shiny fixed

--- demo/demo.R
+++ demo/demo.R
@@ -9,10 +9,66 @@
 
 library(shiny)
 library(reactable)
-library(dplyr)
-library(tibble)
-library(tidyr)
-library(ggplot2)
+library(tidyverse)
+library(rio)
+library(here)
+
+
+safe06 <- import(here("spss_files", "2006_school_safety.sav"),
+                 setclass = "tbl_df") %>% 
+  characterize() %>% 
+  janitor::clean_names() 
+
+safe08 <- import(here('spss_files', '2008_school_safety.sav'),
+                 setclass = 'tbl_df') %>% 
+  characterize() %>% 
+  janitor::clean_names()
+
+safe16 <- import(here("spss_files", "pu_ssocs16.sas7bdat"),
+                 setclass = 'tbl_df') %>% 
+  characterize() %>% 
+  janitor::clean_names() %>% 
+  mutate(fr_ubran = as.factor(fr_urban),
+         fr_size = as.factor(fr_size),
+         year = as.factor(c0578_yy))
+
+safe18 <- import(here('spss_files', '2018_school_safety.sav'),
+                 setclass = 'tbl_df') %>% 
+  characterize() %>% 
+  janitor::clean_names()
+
+
+all_safe <- bind_rows("2006" = safe06, 
+                      "2008" = safe08, 
+                      #"15-16" = safe16,
+                      "2018" = safe18, 
+                      .id = "year")
+
+head(all_safe)
+
+d <- all_safe %>% 
+  select(year, schid, c0508, c0510, c0514, outsus06, outsus08, outsus18, c0534, c0568, fr_catmn, fr_lvel, fr_urban, fr_size)
+
+d <- d %>% 
+  rename(`Total Insubordinations` = c0508,
+         `Total Removals for Insubordination` = c0510,
+         `Total Suspensions for Insubordination` = c0514,
+         `Total Suspensions 2006` = outsus06,
+         `Total Suspensions 2008` = outsus08,
+         `Total Suspensions 2018` = outsus18,
+         `Percentage of College Bound Students` = c0534,
+         `Average Percentage Daily Attendance` = c0568,
+         `Percentage of Minority Enrollment` = fr_catmn,
+         `School Level` = fr_lvel,
+         `School Urbanicity` = fr_urban,
+         `Total Enrollment` = fr_size) 
+
+
+head(d)
+
+d_shiny <- all_safe %>% 
+  select(year, schid, c0508, c0510, c0514, outsus06, outsus08, outsus18, c0534, c0568, fr_catmn, fr_lvel, fr_urban, fr_size) %>% 
+  rename(`Percentage of College Bound Students` = c0534)
 
 theme_set(theme_minimal(15))
 
@@ -57,10 +113,10 @@ ui <- fluidPage(
 server <- function(input, output) {
   d_shiny <- d_shiny %>% 
     mutate(year = factor(year),
-           fr_catmn = factor(fr_catmn),
-           fr_urban = factor(fr_urban),
-           fr_lvel = factor(fr_lvel),
-           fr_size = factor(fr_size))
+           fr_catmn = forcats::fct_explicit_na(fr_catmn),
+           fr_urban = forcats::fct_explicit_na(fr_urban),
+           fr_lvel = forcats::fct_explicit_na(fr_lvel),
+           fr_size = forcats::fct_explicit_na(fr_size))
   
   output$ggplot_dist <- renderPlot({
     
